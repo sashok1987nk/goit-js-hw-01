0a1b15011342b3780231d8f8edb1ca84d060d0a2
sashok1987nik <sashok1987nk@ukr.net>
2023-12-06 07:10:25 +0100

    up

--- js/task-1.js
+++ js/task-1.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 
 function makeTransaction(quantity, pricePerDroid) {
     let totalPrice = quantity * pricePerDroid;
---
+++ js/task-3.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function getElementWidth(content, padding, border) {
+    let n = content + (padding * 2) + (border * 2);
+    return `${n}`
+}
+
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px"));
